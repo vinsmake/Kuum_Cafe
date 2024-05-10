efc758a728cf280346fba192f6a12b4793ae53d0
vinsmake <ps.enrique.e@gmail.com>
2024-05-10 16:29:35 -0600

    Project updated

---
+++ src/components/Kuum_hero.jsx
@@ -0,0 +1,9 @@
+export const Kuum_hero = () => {
+    return (
+        <>
+            <h2>Your favorite Coffee House</h2>
+            <h1>Vinsmake's Cafe template</h1>
+            <h3>Sharing a coffee cup is one of the purest ways of loving</h3>
+        </>
+    )
+}
