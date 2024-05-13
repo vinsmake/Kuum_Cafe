72719d816aab60c6d9e305647a121a1e9e007c58
vinsmake <ps.enrique.e@gmail.com>
2024-05-13 14:09:42 -0600

    Project updated

---
+++ src/components/kuum_menu/Kuum_menu.jsx
@@ -0,0 +1,25 @@
+import "./kuum_menu.css"
+
+
+export const Kuum_menu = () => {
+    return (
+    <>
+        <div className="kuum__menu">
+            <div className="kuum__menu__data">
+                <div className="kuum__menu__data__item">
+                    ey
+                </div>
+                <div className="kuum__menu__data__item">
+                    ey
+                </div>
+                <div className="kuum__menu__data__item">
+                    ey
+                </div>
+                <div className="kuum__menu__data__item">
+                    ey
+                </div>
+            </div>
+        </div>
+    </>
+    )
+}
