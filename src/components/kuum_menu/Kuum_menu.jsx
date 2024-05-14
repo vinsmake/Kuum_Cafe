43c0595fcd4ec5c3d5073ca665038cc6b8ae614c
vinsmake <ps.enrique.e@gmail.com>
2024-05-13 23:11:19 -0600

    Project updated

--- src/components/kuum_menu/Kuum_menu.jsx
+++ src/components/kuum_menu/Kuum_menu.jsx
@@ -1,25 +1,28 @@
 import "./kuum_menu.css"
+import { kuummenudata } from "./kuummenudata"
 
 
 export const Kuum_menu = () => {
+
+    const route = "./images/kuum/menu/"
+
     return (
-    <>
-        <div className="kuum__menu">
-            <div className="kuum__menu__data">
-                <div className="kuum__menu__data__item">
-                    ey
-                </div>
-                <div className="kuum__menu__data__item">
-                    ey
-                </div>
-                <div className="kuum__menu__data__item">
-                    ey
-                </div>
-                <div className="kuum__menu__data__item">
-                    ey
+        <>
+            <div className="kuum__menu">
+                <div className="kuum__menu__data">
+
+                    {
+                        kuummenudata.map(item => (
+                            <div className="kuum__menu__data__item" key={item.title}>
+                                <img src={`${route}${item.title}.jpg`}></img>
+                                <h4>{item.title}</h4>
+                                <p>{item.description}</p>
+                            </div>
+                        ))
+                    }
+
                 </div>
             </div>
-        </div>
-    </>
+        </>
     )
 }
