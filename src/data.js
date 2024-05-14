1fc8bf85dddd46924339a921ecffb7ecc6351f58
vinsmake <ps.enrique.e@gmail.com>
2024-05-14 12:06:22 -0600

    Project updated

--- src/data.js
+++ src/data.js
@@ -18,6 +18,14 @@ import espresso_sensillo from "../src/assets/images/kuum/menu/espresso_sencillo.
 import americano_olla from "../src/assets/images/kuum/menu/americano_olla.png"
 import flat_white from "../src/assets/images/kuum/menu/flat_white.png"
 import cappuccino from "../src/assets/images/kuum/menu/cappuccino.png"
+import moccha from "../src/assets/images/kuum/menu/moccha.png"
+import dirty_chai from "../src/assets/images/kuum/menu/dirty_chai.png"
+import chocolate_kuum from "../src/assets/images/kuum/menu/chocolate_kuum.png"
+import matcha_latte from "../src/assets/images/kuum/menu/matcha_latte.png"
+import iced_matcha_latte from "../src/assets/images/kuum/menu/iced_matcha_latte.png"
+import spring_matcha from "../src/assets/images/kuum/menu/spring_matcha.png"
+import koldbrew from "../src/assets/images/kuum/menu/koldbrew.png"
+import carajillo_kuum from "../src/assets/images/kuum/menu/carajillo_kuum.png"
 export const kuum_menu =
     [
         {
@@ -42,50 +50,42 @@ export const kuum_menu =
         },
         {
             title: 'Moccha',
-            description: "Cappuccino con un pump de chocolate hershey's."
+            description: "Cappuccino con un pump de chocolate hershey's.",
+            img: moccha
         },
         {
             title: 'Dirty Chai',
-            description: 'Mezcla de especias elaborada artesanalmente, leche y espresso doble.'
-        },
-        {
-            title: 'Dirty Matcha',
-            description: 'Matcha latte con vainilla y espresso doble.'
-        },
-        {
-            title: 'Iced Latte',
-            description: 'Espresso doble, leche y mucho hielo.'
-        },
-        {
-            title: 'Iced Matcha Latte',
-            description: 'Matcha con un toque de vainilla, leche y mucho hielo.'
-        },
-        {
-            title: 'Matcha Latte',
-            description: 'Matcha con un toque de vainilla y leche.'
+            description: 'Mezcla de especias elaborada artesanalmente, leche y espresso doble.',
+            img: dirty_chai
         },
         {
             title: 'Chocolate Kuum',
-            description: 'Chocolate rey amargo en agua y espresso doble.'
+            description: 'Chocolate rey amargo en agua y espresso doble.',
+            img: chocolate_kuum
         },
         {
-            title: 'Chai Latte',
-            description: 'Mezcla de especias elaborada en casa y leche.'
+            title: 'Matcha Latte',
+            description: 'Matcha con un toque de vainilla y leche.',
+            img: matcha_latte
+        },
+        {
+            title: 'Iced Matcha Latte',
+            description: 'Matcha con un toque de vainilla, leche y mucho hielo.',
+            img: iced_matcha_latte
         },
         {
             title: 'Spring Matcha',
-            description: 'Sparkling water, zumo de limón, menta, matcha y vainilla con muuuucho hielo.'
+            description: 'Sparkling water, zumo de limón, menta, matcha y vainilla con muuuucho hielo.',
+            img: spring_matcha
         },
         {
             title: 'Koldbrew',
-            description: 'Extracción en frío 36 hrs.'
-        },
-        {
-            title: 'Carajillo de Olla',
-            description: 'El carajillo clásico pero con sabor café de olla.'
+            description: 'Extracción en frío 36 hrs.',
+            img: koldbrew
         },
         {
             title: 'Carajillo Kuum',
-            description: 'Espresso doble, licor artesanal de hierbas y especias, jarabe de olla y koldbrew.'
+            description: 'Espresso doble, licor artesanal de hierbas y especias, jarabe de olla y koldbrew.',
+            img: carajillo_kuum
         }
     ]
