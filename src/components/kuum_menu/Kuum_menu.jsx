831ae0bc0a5500428f6f4ec904fe7ee5d6449788
vinsmake <ps.enrique.e@gmail.com>
2024-05-18 15:36:34 -0600

    Project updated

--- src/components/kuum_menu/Kuum_menu.jsx
+++ src/components/kuum_menu/Kuum_menu.jsx
@@ -6,23 +6,23 @@ import { ButtonRappi } from "../helpers/DeliveryButtons/ButtonRappi"
 
 export const Kuum_menu = () => {
 
-    const route = "./images/kuum/menu/"
+    const {menu, title, legend, link_uber, link_rappi} = kuum_menu;
 
     return (
         <>
             <div className="kuum__menu">
                 <div className="kuum__menu__text">
-                    <h1 className="kuum__menu__text--title">Hay que pedir un café</h1>
-                    <p className="kuum__menu__text--text">Mucho café y brebajes relacionados</p>
+                    <h1 className="kuum__menu__text--title">{title}</h1>
+                    <p className="kuum__menu__text--text">{legend}</p>
                 </div>
                 <div className="kuum__menu__data">
                     
 
                     {
-                        kuum_menu.map(item => (
+                        menu.map(item => (
                             <div className="kuum__menu__data__item" key={item.title}>
                                 <img src={item.img}
-                                /* <img src={imagen} */className="kuum__menu__data__item--img"/>
+                                className="kuum__menu__data__item--img"/>
                                 <div className="kuum__menu__data__item--data">
                                 <h4 className="kuum__menu__data__item--data--title">{item.title}</h4>
                                 <p className="kuum__menu__data__item--data--p">{item.description}</p>
@@ -33,10 +33,10 @@ export const Kuum_menu = () => {
 
                 </div>
                 <div className="kuum__menu__buttons">
-                    <a href="https://www.ubereats.com/store/kuum-cafe-ciudad-guzman/agUSKzQvUxa9A90JoJSJNw?diningMode=DELIVERY" target="_blank" rel="noopener noreferrer">
+                    <a href={link_uber} target="_blank" rel="noopener noreferrer">
                     <ButtonUber/>
                     </a>
-                    <a href="https://www.rappi.com.mx/restaurantes/1930170903-kuum-cafe" target="_blank" rel="noopener noreferrer">
+                    <a href={link_rappi} target="_blank" rel="noopener noreferrer">
                     <ButtonRappi/>
                     </a>
                 </div>
