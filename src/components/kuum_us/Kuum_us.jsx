28a41763e676006ce4d31260ba86c683b23c538d
vinsmake <ps.enrique.e@gmail.com>
2024-05-18 12:59:35 -0600

    Project updated

--- src/components/kuum_us/Kuum_us.jsx
+++ src/components/kuum_us/Kuum_us.jsx
@@ -5,27 +5,27 @@ import { kuum_us } from "../../data";
 
 export const Kuum_us = () => {
 
-    const {spinning_plate, kicker, title, text} = kuum_us
+    const { spinning_plate, kicker, title, text } = kuum_us
 
     const imgAnimation = useScrollAnimation('roll-to-left 2s ease-out both', 'roll-to-right 2s ease-out both');
 
     return (
-    <>
-        <div className="kuum__us">
-            <div className="kuum__us__data">
-            <div className="kuum__us__data__text">
-                <h4 className="kuum__us__data__text--span">{kicker}</h4>
-                <h2 className="kuum__us__data__text--title">{title}</h2>
-                <p className="kuum__us__data__text--text">{text}</p>
+        <>
+            <div className="kuum__us">
+                <div className="kuum__us__data">
+                    <div className="kuum__us__data__text">
+                        <h4 className="kuum__us__data__text--span">{kicker}</h4>
+                        <h2 className="kuum__us__data__text--title">{title}</h2>
+                        <p className="kuum__us__data__text--text">{text}</p>
+                    </div>
+                    <div className="kuum__us__data__image">
+                        <img src={spinning_plate} className="kuum__us__data__image--src"
+                            title="Photo by Nathan Dumlao on Unsplash"
+                            ref={imgAnimation}
+                        />
+                    </div>
                 </div>
-            <div className="kuum__us__data__image">
-                <img src={spinning_plate} className="kuum__us__data__image--src" 
-                title="Photo by Nathan Dumlao on Unsplash"
-                ref={imgAnimation}
-                />
             </div>
-            </div>
-        </div>
-    </>
+        </>
     )
 }
