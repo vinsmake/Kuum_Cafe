f912242cf12496a2cc5eb707098cc893dd21a984
vinsmake <ps.enrique.e@gmail.com>
2024-05-19 16:03:07 -0600

    Project updated

--- src/components/kuum_clients/Kuum_clients.jsx
+++ src/components/kuum_clients/Kuum_clients.jsx
@@ -2,8 +2,48 @@ import "./kuum_clients.css"
 
 export const Kuum_clients = () => {
     return (
-    <>
-        <h1 className="kuum__clients">Kuum_clients</h1>
-    </>
-    )
+
+        <div class="slider">
+            <input type="radio" name="testimonial" id="t-1"/>
+                <input type="radio" name="testimonial" id="t-2"/>
+                    <input type="radio" name="testimonial" id="t-3" checked/>
+                        <input type="radio" name="testimonial" id="t-4"/>
+                            <input type="radio" name="testimonial" id="t-5"/>
+                                <div class="testimonials">
+                                    <label class="item" for="t-1">
+                                        <img src="https://dummyimage.com/150" alt="picture"/>
+                                            <p>"Raw denim you probably haven't heard of them jean short austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse."</p>
+                                            <h2>- Princy, Web Developer</h2>
+                                    </label>
+                                    <label class="item" for="t-2">
+                                        <img src="https://dummyimage.com/150" alt="picture"/>
+                                            <p>"Raw denim you probably haven't heard of them jean short austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse."</p>
+                                            <h2>- Princy, Web Developer</h2>
+                                    </label>
+                                    <label class="item" for="t-3">
+                                        <img src="https://dummyimage.com/150" alt="picture"/>
+                                            <p>"Raw denim you probably haven't heard of them jean short austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse."</p>
+                                            <h2>- Princy, Web Developer</h2>
+                                    </label>
+                                    <label class="item" for="t-4">
+                                        <img src="https://dummyimage.com/150" alt="picture"/>
+                                            <p>"Raw denim you probably haven't heard of them jean short austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse."</p>
+                                            <h2>- Princy, Web Developer</h2>
+                                    </label>
+                                    <label class="item" for="t-5">
+                                        <img src="https://dummyimage.com/150" alt="picture"/>
+                                            <p>"Raw denim you probably haven't heard of them jean short austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse."</p>
+                                            <h2>- Princy, Web Developer</h2>
+                                    </label>
+                                </div>
+                                <div class="dots">
+                                    <label for="t-1"></label>
+                                    <label for="t-2"></label>
+                                    <label for="t-3"></label>
+                                    <label for="t-4"></label>
+                                    <label for="t-5"></label>
+                                </div>
+                            </div>
+
+                            )
 }
